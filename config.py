f131bb16c68650cc64c821e3f92c489eebf138bf
sunnysab <i@sunnysab.cn>
2025-02-01 09:37:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 import os
 
 
@@ -6,7 +5,9 @@ __DEFAULT_DEEPSEEK_KEY = 'your-deepseek-key'
 __DEFAULT_CHATGLM_KEY = 'your-chatglm-key'
 __DEFAULT_OLLAMA_HOST = 'http://localhost:11434'
 __WCF_HOST = '192.168.2.105'
-__WCF_PORT = 10086
+__WCF_PORT = 5000
+__REMOTE_STORAGE_PATH = '.'
+__REMOTE_SERVER_PREFIX = 'http://192.168.2.105:5001'
 
 CONFIG = {
     'deepseek-key': os.getenv('DEEPSEEK_KEY', __DEFAULT_DEEPSEEK_KEY),
@@ -14,5 +15,6 @@ CONFIG = {
     'ollama-host': os.getenv('OLLAMA_HOST', __DEFAULT_OLLAMA_HOST),
     'wcf-host': os.getenv('WCF_HOST', __WCF_HOST),
     'wcf-port': int(os.getenv('WCF_PORT', __WCF_PORT)),
+    'remote-storage-path': os.getenv('REMOTE_STORAGE_PATH', __REMOTE_STORAGE_PATH),
+    'remote-server-prefix': os.getenv('REMOTE_SERVER_PREFIX', __REMOTE_SERVER_PREFIX),
 }
-
